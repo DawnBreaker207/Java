4347855e61e71274491032df7df658cd249e9f58
DawnBreaker <tunganhngo207@gmail.com>
2024-12-27 17:04:30 +0700

    feat: add chapter 1

---
+++ src/Chapter1/DooBee.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+public class DooBee {
+	public static void main(String[] args) {
+		int x = 1;
+		while (x <= 10) {
+			System.out.println("Doo");
+			System.out.println("Bee");
+			x = x +1;
+		}
+		if(x == 11) {
+			System.out.print("Do");
+		}
+	}
+}
+
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import Chapter1.DooBee;
+import Chapter1.BottleSong;
+import Chapter1.PhraseOMatic;
+/**
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+//	    DooBee.main(args);
+//	    BottleSong.main(args);
+	    PhraseOMatic.main(args);
+	}
+
+}
