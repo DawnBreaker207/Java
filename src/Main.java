65155d17b7fac25898710706d31ef22aefec49ac
DawnBreaker <tunganhngo207@gmail.com>
2024-12-27 20:21:04 +0700

    feat: update code chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import Chapter1.DooBee;
 import Chapter1.BottleSong;
 import Chapter1.PhraseOMatic;
+import Chapter1.Shufflel;
+import Chapter1.PoolPuzzleOne;
 /**
  * 
  */
@@ -9,7 +11,9 @@ public class Main {
 	public static void main(String[] args) {
 //	    DooBee.main(args);
 //	    BottleSong.main(args);
-	    PhraseOMatic.main(args);
+//	    PhraseOMatic.main(args);
+//	    Shufflel.main(args);
+	    PoolPuzzleOne.main(args);
 	}
 
 }
