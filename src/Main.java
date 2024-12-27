aab7f8085cd06987408401a76d5b56ae02018372
DawnBreaker <tunganhngo207@gmail.com>
2024-12-27 20:22:59 +0700

    feat: update

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import Chapter1.DooBee;
-import Chapter1.BottleSong;
-import Chapter1.PhraseOMatic;
-import Chapter1.Shufflel;
-import Chapter1.PoolPuzzleOne;
-/**
- * 
- */
 public class Main {
 
 	public static void main(String[] args) {
-//	    DooBee.main(args);
-//	    BottleSong.main(args);
-//	    PhraseOMatic.main(args);
-//	    Shufflel.main(args);
-	    PoolPuzzleOne.main(args);
+
 	}
 
 }
