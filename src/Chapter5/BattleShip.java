8db488a0356987ac683a23dc21c18669e6489f42
DawnBreaker <tunganhngo207@gmail.com>
2024-12-31 20:38:36 +0700

    feat: add code

---
+++ src/Chapter5/BattleShip.java
@@ -0,0 +1,16 @@
+package Chapter5;
+class Game{
+    
+}
+class Startup{
+    int [] locationCells;
+    int numOfHits;
+}
+public class BattleShip {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
