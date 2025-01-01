9da4225617659b0ac5bdf84e89e29cc1ab1a7270
DawnBreaker <tunganhngo207@gmail.com>
2025-01-01 14:50:11 +0700

    feat: add chapter 5

--- src/Chapter5/BattleShip.java
+++ src/Chapter5/BattleShip.java
@@ -1,16 +1,54 @@
 package Chapter5;
-class Game{
-    
+
+import java.util.Iterator;
+
+class Game {
+
 }
-class Startup{
-    int [] locationCells;
-    int numOfHits;
+
+class Startup {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    String checkYourSelf(int guess) {
+	String result = "miss";
+	for (int cell : locationCells) {
+	    if (guess == cell) {
+		result = "hit";
+		numOfHits++;
+		break;
+	    }
+	}
+	if (numOfHits == locationCells.length) {
+	    result = "kill";
+	}
+	System.out.println(result);
+	return result;
+
+    }
+
+    void setLocationCells(int[] location) {
+	locationCells = location;
+    }
 }
+
 public class BattleShip {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	Startup dot = new Startup();
 
+	int[] locations = { 2, 3, 4 };
+	dot.setLocationCells(locations);
+
+	int userGuess = 3;
+	String result = dot.checkYourSelf(userGuess);
+
+	String testResult = "failed";
+	if (result.equals("hit")) {
+	    testResult = "passed";
+	}
+
+	System.out.println(testResult);
     }
 
 }
