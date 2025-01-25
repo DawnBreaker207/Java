6d1e4179ff530e09b6f28ef1b79a44685072ee14
DawnBreaker <tunganhngo207@gmail.com>
2025-01-25 17:39:11 +0700

    feat: update code

---
+++ src/Chapter11/Jukebox3.java
@@ -0,0 +1,19 @@
+package Chapter11;
+
+import java.util.Collections;
+import java.util.List;
+
+class Jukebox3 {
+    public static void main(String[] args) {
+	new Jukebox3().go();
+    }
+
+    public void go() {
+	List<SongV3> songList = MockSongs.getSongV3();
+	System.out.println(songList);
+	Collections.sort(songList);
+	System.out.println(songList);
+
+    }
+
+}
