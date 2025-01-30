08ec25c03b059065d7495c2fc8f5a73aad06b749
DawnBreaker <tunganhngo207@gmail.com>
2025-01-30 21:27:51 +0700

    feat: update code

--- src/Chapter11/Jukebox8.java
+++ src/Chapter11/Jukebox8.java
@@ -8,14 +8,61 @@ class Jukebox8 {
     }
 
     public void go() {
-	List<SongV3> songList = MockMoreSongs.getSongV3();
+	List<SongV4> songList = MockMoreSongs.getSongV4();
 	System.out.println(songList);
 
 	songList.sort((one, two) -> one.getTitle().compareTo(two.getTitle()));
 	System.out.println(songList);
 
-	Set<SongV3> songSet = new HashSet<>(songList);
+//	Set<SongV4> songSet = new HashSet<>(songList);
+//	Set<SongV4> songSet = new TreeSet<>(songList);
+	Set<SongV4> songSet = new TreeSet<>((o1, o2) -> o1.getBpm() - o2.getBpm());
+	songSet.addAll(songList);
 	System.out.println(songSet);
+    }
+
+}
+
+class SongV4 implements Comparable<SongV4> {
+    private String title;
+    private String artist;
+    private int bpm;
+  
+    public boolean equals(Object aSong) {
+	SongV4 other = (SongV4) aSong;
+	return title.equals(other.getTitle());
+    }
+
+    public int hashCode() {
+	return title.hashCode();
+    }
+
+    public int compareTo(SongV4 s) {
+	return title.compareTo(s.getTitle());
+    }
+
+    public String toString() {
+	return title;
+    }
+
+    SongV4(String title, String artist, int bpm) {
+	this.title = title;
+	this.artist = artist;
+	this.bpm = bpm;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public String getArtist() {
+	return artist;
+
+    }
+
+    public int getBpm() {
+	return bpm;
+
     }
 
 }
