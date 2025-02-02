a444443d01c4c3a214c152c67c1e66128029a02d
DawnBreaker <tunganhngo207@gmail.com>
2025-02-02 15:27:12 +0700

    feat: update chapter 12

---
+++ src/Chapter12/MixForLoops.java
@@ -0,0 +1,29 @@
+package Chapter12;
+
+import java.util.*;
+
+public class MixForLoops {
+
+    public static void main(String[] args) {
+	List<Integer> nums = List.of(1, 2, 3, 4, 5);
+	String output = "";
+//	for (Integer num : nums) {
+//	    output += nums + " ";
+//	}
+
+	for (int i = 1; i < nums.size(); i++) {
+	    output += nums.get(i) + " ";
+	}
+
+//	for (int i = 0; i <= nums.length; i++) {
+//	    output += nums.get(i) + " ";
+//	}
+
+//	for(int i = 0; i<= nums.size(); i ++) {
+//	    output += nums.get(i) + " ";
+//	}
+//	
+	System.out.println(output);
+    }
+
+}
